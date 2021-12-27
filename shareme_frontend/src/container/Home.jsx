321648a4c7fe5e67357e56dd11efa51ee5597565
ayushmukho <ayushmukho@gmail.com>
2021-12-27 13:24:02 +0530

    Completed Sidebar

--- shareme_frontend/src/container/Home.jsx
+++ shareme_frontend/src/container/Home.jsx
@@ -6,9 +6,9 @@ import { Link, Route, Routes } from "react-router-dom";
 import { client } from "../client";
 import { userQuery } from "../utils/data";
 
-import { Sidebar, UserProfile } from "../components"
-import Pins from "./Pins"
-import logo from "../assets/logo.png"
+import { Sidebar, UserProfile } from "../components";
+import Pins from "./Pins";
+import logo from "../assets/logo.png";
 
 const Home = () => {
   const [toggleSidebar, setToggleSidebar] = useState(false);
@@ -23,11 +23,14 @@ const Home = () => {
   useEffect(() => {
     const query = userQuery(userInfo?.googleId);
 
-    client.fetch(query)
-      .then((data) => {
-        setUser(data[0]);
-      })
-  }, [])
+    client.fetch(query).then((data) => {
+      setUser(data[0]);
+    });
+  }, []);
+
+  useEffect(() => {
+    scrollRef.current.scrollTo(0, 0);
+  }, []);
 
   return (
     <div className="flex bg-gray-50 md:flex-row flex-col h-screen transaction-height duration-75 ease-out">
@@ -35,22 +38,38 @@ const Home = () => {
         <Sidebar user={user && user} />
       </div>
       <div className="flex md:hidden flex-row">
-        <HiMenu fontSize={40} className="cursor-pointer" onClick={() => setToggleSidebar(true)} />
-        <Link to="/">
-          <img src={logo} alt="logo" className="w-28" />
-        </Link>
-        <Link to={`user-profile/${user?._id}`}>
-          <img src={user?.image} alt="logo" className="w-28" />
-        </Link>
-      </div>
-      {toggleSidebar && (
-        <div className="fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in">
-          <div className="absolute w-full flex justify-end items-center p-2">
-            <AiFillCloseCircle fontSize={30} className="cursor-pointer" onClick={() => setToggleSidebar(false)} />
-          </div>
-          <Sidebar user={user && user} />
+        <div className="p-2 w-full flex flex-row justify-between items-center shadow-md">
+          <HiMenu
+            fontSize={40}
+            className="cursor-pointer"
+            onClick={() => setToggleSidebar(true)}
+          />
+          <Link to="/">
+            <img src={logo} alt="logo" className="w-28" />
+          </Link>
+          <Link to={`user-profile/${user?._id}`}>
+            <img src={user?.image} alt="logo" className="w-28" />
+          </Link>
         </div>
-      )}
+        {toggleSidebar && (
+          <div className="fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in">
+            <div className="absolute w-full flex justify-end items-center p-2">
+              <AiFillCloseCircle
+                fontSize={30}
+                className="cursor-pointer"
+                onClick={() => setToggleSidebar(false)}
+              />
+            </div>
+            <Sidebar user={user && user} closeToggle={setToggleSidebar} />
+          </div>
+        )}
+      </div>
+      <div className="pb-2 flex-1 h-screen overflow-y-scroll" ref={scrollRef}>
+        <Routes>
+          <Route path="/user-profile/:userId" element={<UserProfile />} />
+          <Route path="/*" element={<Pins user={user && user} />} />
+        </Routes>
+      </div>
     </div>
   );
 };
--- shareme_frontend/src/container/Pins.jsx
+++ shareme_frontend/src/container/Pins.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Pins = () => {
   return (
     <div>
-      Display Pins
+      Pins
     </div>
   )
 }
